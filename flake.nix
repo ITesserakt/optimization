a481e3155dcc3bea79426652bf51042d6a7f922a
ITesserakt <portyas85@mail.ru>
2025-03-06 09:56:04 +0300

    Added shell for nixos

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, fenix }: let 
+	system = "x86_64-linux";
+	pkgs = import nixpkgs { inherit system; };
+	toolchain = fenix.packages.${system}.latest.toolchain;
+  in {
+	devShells.${system}.default = pkgs.mkShell {
+	  packages = with pkgs; [
+	  	toolchain
+	  ];
+	};
+  };
+}
